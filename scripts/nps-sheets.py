d5b8434e97a55d7e672cd65e8d4c2e94ae0ebaab
prempiyush <prempiyush@in.ibm.com>
2017-05-21 15:18:22 +0530

    Minor changes

--- scripts/nps-sheets.py
+++ scripts/nps-sheets.py
@@ -1,4 +1,5 @@
 import gspread
+import logging
 import requests
 import zipfile
 import os
@@ -9,9 +10,15 @@ from utils import csv2list
 
 dateFormat = "%d%m%Y"
 schemes = ('SM001001', 'SM001002', 'SM001003', 'SM001004', 'SM001005', 'SM001006', 'SM001007', 'SM001008', 'SM001009', 'SM001010', 'SM001011', 'SM001012', 'SM001013', 'SM002001', 'SM002002', 'SM002003', 'SM002004', 'SM002005', 'SM002006', 'SM002007', 'SM002008', 'SM002009', 'SM002010', 'SM002011', 'SM002012', 'SM002013', 'SM003001', 'SM003002', 'SM003003', 'SM003004', 'SM003005', 'SM003006', 'SM003007', 'SM003008', 'SM003009', 'SM003010', 'SM003011', 'SM003012', 'SM003013', 'SM005001', 'SM005002', 'SM005003', 'SM005004', 'SM005005', 'SM005006', 'SM005007', 'SM005008', 'SM005009', 'SM006001', 'SM006002', 'SM006003', 'SM006004', 'SM006005', 'SM006006', 'SM006007', 'SM006008', 'SM006009', 'SM007001', 'SM007002', 'SM007003', 'SM007004', 'SM007005', 'SM007006', 'SM007007', 'SM007008', 'SM007009', 'SM008001', 'SM008002', 'SM008003', 'SM008004', 'SM008005', 'SM008006', 'SM008007', 'SM008008', 'SM008009', 'SM010001', 'SM010002', 'SM010003', 'SM010004', 'SM010005', 'SM010006', 'SM010007', 'SM010008')
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%d-%b-%Y %H:%M',
+                    filename='nps.log',
+                    filemode='a')
+logger = logging.getLogger(__name__)
 
 def outFileToFormattedRow(fileName):
-    print("outFileToFormattedRow: Entering.. Have to format " + fileName)
+    logger.debug("outFileToFormattedRow: Entering.. Have to format " + fileName)
     fileDate = datetime.strptime(fileName.split("_")[2].split(".")[0], dateFormat)
     rows = csv2list(fileName)
 
@@ -24,26 +31,26 @@ def outFileToFormattedRow(fileName):
     for row in rows:
         firstCol = datetime.strptime(row[0], '%m/%d/%Y')
         if not firstCol == fileDate:
-            print("outFileToFormattedRow: File for " + fileDate.strftime("%d/%m/%Y") + " has an entry for " + firstCol.strftime("%d/%m/%Y"))
+            logger.error("outFileToFormattedRow: File for " + fileDate.strftime("%d/%m/%Y") + " has an entry for " + firstCol.strftime("%d/%m/%Y"))
             continue
         i = -1
         newRow[0] = fileDate.strftime('%d/%m/%Y')
         try:
-            #print(row)
+            #logger.debug(row)
             i = schemes.index(row[3])
         except Exception as e:
-            print("outFileToFormattedRow: No scheme id found. New Scheme?")
+            logger.error("outFileToFormattedRow: No scheme id found. New Scheme?")
             # TODO needs more work. Perhaps email if this error?
         else:
             newRow[i + 1] = float(row[5])
-    print("outFileToFormattedRow: Deleting file: " + fileName)
+    logger.debug("outFileToFormattedRow: Deleting file: " + fileName)
     os.remove(fileName)
-    print("outFileToFormattedRow: Returning row: " + newRow)
+    logger.debug("outFileToFormattedRow: Returning row: " + str(newRow))
     return(newRow)
 
 def downloadDataFromNPS(fileDate):
     fileName = "NAV_File_"+fileDate.strftime(dateFormat)+".zip"
-    print("downloadDataFromNPS: Entering.. Have to download " + fileName)
+    logger.debug("downloadDataFromNPS: Entering.. Have to download " + fileName)
 
     headers = {
         'Accept-Encoding': 'gzip, deflate, sdch, br',
@@ -59,20 +66,20 @@ def downloadDataFromNPS(fileDate):
     result = requests.get('https://npscra.nsdl.co.in/download.php', headers=headers, params=params)
 
     if(result.content):
-        print("downloadDataFromNPS: File " + fileName + " downloaded. Saving")
+        logger.debug("downloadDataFromNPS: File " + fileName + " downloaded. Saving")
         zfile = open(fileName, 'wb')
         zfile.write(result.content)
         zfile.close()
         zip_ref = zipfile.ZipFile(fileName, 'r')
-        print("downloadDataFromNPS: Extracting File " + fileName)
+        logger.debug("downloadDataFromNPS: Extracting File " + fileName)
         zip_ref.extractall()
         outFile = zip_ref.namelist()[0]
         zip_ref.close()
-        print("downloadDataFromNPS: Extracting File " + fileName)
+        logger.debug("downloadDataFromNPS: Extracting File " + fileName)
         os.remove(fileName)
-        print("downloadDataFromNPS: Returning " + outFile)
+        logger.debug("downloadDataFromNPS: Returning " + outFile)
         return(outFile)
-    print("downloadDataFromNPS: No data returned from NPS. Returning False")
+    logger.debug("downloadDataFromNPS: No data returned from NPS. Returning False")
     return(False)
 
 
@@ -86,7 +93,7 @@ def appendRow(endDate = ''):
     if(endDate):
         endDate = datetime.strptime(endDate, "%d/%m/%Y")
     else:
-        endDate = date.today()
+        endDate = datetime.today()
     rowCount = wks.row_count
     lastRow = wks.row_values(rowCount)
     lastDate = datetime.strptime(lastRow[0],"%d/%m/%Y")
@@ -96,15 +103,22 @@ def appendRow(endDate = ''):
         if(fileName):
             values = outFileToFormattedRow(fileName)
         else:
-            print("appendRow: No NAV on " + currentDate.strftime(dateFormat))
-            print("appendRow: Using NAV from " + lastDate.strftime(dateFormat))
+            logger.debug("appendRow: No NAV on " + currentDate.strftime(dateFormat))
+            logger.debug("appendRow: Using NAV from " + lastDate.strftime(dateFormat))
             values = lastRow
             values[0] = currentDate.strftime("%d/%m/%Y")
-        wks.append_row(values)
-        print("Values for " + currentDate + " updated")
+        wks.add_rows(1)
         rowCount = rowCount + 1
+        cellList = wks.range(rowCount, 1, rowCount, len(values))
+        i = 0
+        for cell in cellList:
+            cell.value = values[i]
+            i = i + 1
+        #wks.append_row(values)
+        wks.update_cells(cellList)
+        logger.debug("Values for " + currentDate.strftime(dateFormat) + " updated")
         lastRow = values
         lastDate = currentDate
-        currentDate = currentDate + 1
+        currentDate = currentDate + timedelta(1)
 
 appendRow()
--- scripts/utils.py
+++ scripts/utils.py
@@ -4,5 +4,9 @@ import csv
 def csv2list(fileName):
     with open(fileName, 'r') as f:
         reader = csv.reader(f)
-        rows = list(reader)
+        data = f.read()
+        new_data = data.replace('"', '')
+        rows = []
+        for row in csv.reader(new_data.splitlines(), delimiter=',', skipinitialspace=True):
+            rows.append(row)
         return(rows)
